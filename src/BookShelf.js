7d8de6bb33eeeb7f9774cf97471cd52a3393e855
tschneweis2 <tschneweis95@outlook.com>
2020-01-11 23:17:30 -0600

    feat: Created ListBooks Component
    Created a ListBooks component. This component will list books for each bookshelf.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import ListBooks from './ListBooks';
 class BookShelf extends Component{
     render(){
         const { category } = this.props;
         return(<div className="bookshelf">
         <h2 className="bookshelf-title">{category}</h2>
-        
+        <ListBooks />
       </div>)
     }
 }
---
+++ src/ListBooks.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+class ListBooks extends Component{
+    render(){
+        return(
+<div className="bookshelf-books">
+                    <ol className="books-grid">
+                     
+                    
+                    </ol>
+                  </div>
+
+        )
+    }
+}
+export default ListBooks;
