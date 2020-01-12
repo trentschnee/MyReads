6d193f7387b4c79299f6e0bd9aeb27591758eea0
tschneweis2 <tschneweis95@outlook.com>
2020-01-12 00:09:45 -0600

    FEAT:  Added allBooks State
    Created an allBooks state and passed the data via props to BookShelf, from there, all books are passed down via props to listBooks. Lastly, ListBooks loops through all of the books and displays them with the Book Component. This prop will be critical when displaying books by the chosen category.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react';
 import ListBooks from './ListBooks';
 class BookShelf extends Component{
     render(){
-        const { category } = this.props;
+        const { category, allBooks } = this.props;
         return(<div className="bookshelf">
         <h2 className="bookshelf-title">{category}</h2>
-        <ListBooks />
+        <ListBooks books={allBooks} />
       </div>)
     }
 }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
+import Book from './Book';
 class ListBooks extends Component{
     render(){
         return(
 <div className="bookshelf-books">
                     <ol className="books-grid">
-                     
-                    
+                     {this.props.books.map((book, index)=><Book key={index} book={book}/>)}
                     </ol>
                   </div>
 
